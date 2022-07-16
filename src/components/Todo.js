4ea13998fb3a86bad89123251f73576d4e9d4c92
Avishek Das <davishek7@gmail.com>
2022-07-16 09:13:21 +0530

    Starting on components

---
+++ src/components/Todo.js
@@ -0,0 +1,14 @@
+function Todo() {
+    return (
+        <div className="card">
+            <div>
+                <h2 className="card-body">Title</h2>
+                <div>
+                    <button className="btn btn-danger">Delete</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Todo;
